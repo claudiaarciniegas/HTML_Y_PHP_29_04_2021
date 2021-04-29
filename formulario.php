1d32edc4132fc8933d67b3cd7138ca20158629a0
claudiaarciniegas <claudiaaarciniegas83@gmail.com>
2021-04-29 17:15:09 -0500

    HTML_Y_PHP_29_ 04_2021

---
+++ formulario.php
@@ -0,0 +1,22 @@
+<?php
+   echo <<<TIPO
+   Tu Tipo de Identificaión: $_REQUEST[Ident]<br>
+TIPO;
+   echo <<<IDENTIFICACION
+   Tu Identificaión es: $_REQUEST[id]<br>
+IDENTIFICACION;
+   echo <<<NOMBRE
+   Tu nombre es: $_REQUEST[nombre]<br>
+NOMBRE;
+   echo <<<EDAD
+   Tu edad es: $_REQUEST[edad]<br>
+EDAD;
+   ECHO <<<GENERO
+   Tu genero es: $_REQUEST[Genero]<br>
+GENERO;
+   echo <<<ESTADO
+   Tu Estado civil es: $_REQUEST[estado]<br>
+ESTADO;
+
+
+?>
